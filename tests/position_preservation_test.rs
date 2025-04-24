069b186fe95e0b1216f2316d61d7c08d0725e6a9
Kris Nuttycombe <kris@nutty.land>
2025-04-24 16:03:56 -0600

    Add github continuous integration builds.

--- tests/position_preservation_test.rs
+++ tests/position_preservation_test.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use zewif::{Transaction, Position};
+use zewif::{Position, Transaction};
 
 // Test that positions are non-zero after migration
 #[test]
@@ -8,44 +8,61 @@ fn test_position_preservation() -> Result<()> {
     let txid_bytes = [0u8; 32]; // zeroed txid for testing
     let txid = zewif::TxId::from_bytes(txid_bytes);
     let mut tx = Transaction::new(txid);
-    
+
     // Add a sapling output with a commitment
     let mut sapling_output = zewif::sapling::SaplingOutputDescription::new();
-    let default_commitment = zewif::u256::from_hex("0000000000000000000000000000000000000000000000000000000000000000");
+    let default_commitment =
+        zewif::u256::from_hex("0000000000000000000000000000000000000000000000000000000000000000");
     sapling_output.set_commitment(default_commitment);
-    
+
     // Initially the position is 0 (default)
-    assert_eq!(u32::from(*sapling_output.note_commitment_tree_position()), 0, 
-              "Position should start at zero");
-              
+    assert_eq!(
+        u32::from(*sapling_output.note_commitment_tree_position()),
+        0,
+        "Position should start at zero"
+    );
+
     // Set a non-zero position
     sapling_output.set_note_commitment_tree_position(Position::from(42u32));
-    assert_eq!(u32::from(*sapling_output.note_commitment_tree_position()), 42, 
-              "Position should be updated to 42");
-    
+    assert_eq!(
+        u32::from(*sapling_output.note_commitment_tree_position()),
+        42,
+        "Position should be updated to 42"
+    );
+
     // Add an orchard action with a commitment
     let mut orchard_action = zewif::OrchardActionDescription::new();
-    let commitment = zewif::u256::from_hex("0101010101010101010101010101010101010101010101010101010101010101");
+    let commitment =
+        zewif::u256::from_hex("0101010101010101010101010101010101010101010101010101010101010101");
     orchard_action.set_commitment(commitment);
-    
+
     // Set a non-zero position
     orchard_action.set_note_commitment_tree_position(Position::from(123u32));
-    assert_eq!(u32::from(*orchard_action.note_commitment_tree_position()), 123, 
-              "Position should be updated to 123");
-              
+    assert_eq!(
+        u32::from(*orchard_action.note_commitment_tree_position()),
+        123,
+        "Position should be updated to 123"
+    );
+
     // Add them to the transaction
     tx.add_sapling_output(sapling_output);
     tx.add_orchard_action(orchard_action);
-    
+
     // Now verify that the positions are maintained in the transaction
     let outputs = tx.sapling_outputs().unwrap();
     let actions = tx.orchard_actions().unwrap();
-    
-    assert_eq!(u32::from(*outputs[0].note_commitment_tree_position()), 42, 
-              "Sapling output position should be preserved");
-    assert_eq!(u32::from(*actions[0].note_commitment_tree_position()), 123, 
-              "Orchard action position should be preserved");
-    
+
+    assert_eq!(
+        u32::from(*outputs[0].note_commitment_tree_position()),
+        42,
+        "Sapling output position should be preserved"
+    );
+    assert_eq!(
+        u32::from(*actions[0].note_commitment_tree_position()),
+        123,
+        "Orchard action position should be preserved"
+    );
+
     // This verifies that our position update logic works correctly
     Ok(())
 }
